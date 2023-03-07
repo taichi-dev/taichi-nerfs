352b921cbef4a8e5e0a37f3df9ca49956e193417
Zhanlue Yang <zy2284@columbia.edu>
2023-03-07 10:28:28 +0800

    Update train_nsvf_lego.sh

--- scripts/train_nsvf_lego.sh
+++ scripts/train_nsvf_lego.sh
@@ -5,6 +5,6 @@ export DATA_DIR=./Synthetic_NeRF
 python3 train.py \
     --root_dir $DATA_DIR/Lego \
     --exp_name Lego --perf \
-    --num_epochs 20 --batch_size 8192 --lr 1e-2 --no_save_test \
-    --half2_opt --gui\
+    --num_epochs 20 --batch_size 8192 --lr 1e-2 --no_save_test --gui \
+    #--half2_opt \
     #--val_only --ckpt_path ckpts/nsvf/Lego/epoch=19-v6.ckpt
