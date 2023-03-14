1c4be36b4aaaad12936a036def981f314cd97502
Youtian Lin <linyoutian.loyot@gmail.com>
2023-03-14 17:36:30 +0800

    add ngp datasets (#20)

---
+++ scripts/train_ngp_dataset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+export ROOT_DIR="./ngp_data"
+export DOWNSAMPLE=0.5 # to avoid OOM
+
+python3 train.py \
+    --root_dir $ROOT_DIR/ --dataset_name ngp \
+    --exp_name custom_ngp --downsample $DOWNSAMPLE \
+    --num_epochs 20 --scale 16.0 --batch_size 8192
